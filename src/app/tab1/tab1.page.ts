351169609459e2a4dc79619a3b78479edaf3da5a
Celline <56645497+celllliiiinnneeeee@users.noreply.github.com>
2019-11-17 22:24:35 +0700

    celline tambah sesuatu

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -114,7 +114,11 @@ export class Tab1Page {
     // this._router.navigateByUrl('product-detail-preorder');
     this._navCtrl.navigateForward('product-detail-preorder');
   }
-
+  
+  opennegara() {
+    // this._router.navigateByUrl('negara');
+    this._navCtrl.navigateForward('negara');
+  }
 
 
 
